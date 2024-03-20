78c079f5109b52782052f33f7a83c9c6301b44f5
Dvôrakcs <Mhsf15@hotmail.com>
2024-03-20 17:02:33 -0300

    Arrange | Act | Assert -> AAA - patrao definido para teste unitario

--- TesteConta/Program.cs
+++ TesteConta/Program.cs
@@ -9,11 +9,14 @@ class ContaTeste
 
     private static void TesteContaSacarSemSaldo()
     {
+        //arrange
         var conta = new Conta("0001", 0);
         bool resultadoEsperado = false;
 
+        //act
         bool resultado = conta.Sacar(50);
 
+        //assert
         if (resultado == resultadoEsperado)
         {
             Console.WriteLine("TestarContaSacar: Success");
