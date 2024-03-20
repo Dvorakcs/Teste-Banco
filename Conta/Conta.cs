e14961dc2ebc043fa76639c5335d08e9c149aa36
Dvôrakcs <Mhsf15@hotmail.com>
2024-03-20 16:56:30 -0300

    Adicionar arquivos de projeto.

---
+++ Conta/Conta.cs
@@ -0,0 +1,34 @@
+﻿
+    public class Conta
+    {
+        private string cpf;
+        private decimal saldo;
+        public Conta(string cpf, decimal saldo)
+        {
+            this.cpf = cpf;
+            this.saldo = saldo;
+
+        }
+
+        public decimal GetSaldo()
+        {
+            return saldo;
+        }
+
+        public void Depositar(decimal saldo)
+        {
+            this.saldo += saldo;
+        }
+
+        public bool Sacar(decimal valor)
+        {
+            if(valor > this.saldo)
+            {
+               return false;
+            }
+              this.saldo -= saldo;
+              return true;
+        
+        }
+    }
+
---
+++ TesteConta/Program.cs
@@ -0,0 +1,43 @@
+﻿
+class ContaTeste
+{
+    static void Main(string[] args)
+    {
+        TesteContaSacar();
+        TesteContaSacarSemSaldo();
+    }
+
+    private static void TesteContaSacarSemSaldo()
+    {
+        var conta = new Conta("0001", 0);
+        bool resultadoEsperado = false;
+
+        bool resultado = conta.Sacar(50);
+
+        if (resultado == resultadoEsperado)
+        {
+            Console.WriteLine("TestarContaSacar: Success");
+        }
+        else
+        {
+            Console.WriteLine("TestarContaSacar: Failled");
+        }
+    }
+
+    private static void TesteContaSacar()
+    {
+        var conta = new Conta("0001", 100);
+        bool resultadoEsperado = true;
+
+        bool resultado = conta.Sacar(50);
+
+        if(resultado == resultadoEsperado)
+        {
+            Console.WriteLine("TestarContaSacar: Success");
+        }
+        else
+        {
+            Console.WriteLine("TestarContaSacar: Failled");
+        }
+    }
+}
